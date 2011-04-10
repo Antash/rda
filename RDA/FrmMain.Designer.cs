7330ae6131085326a051eba745292d6bfebe61ee
Anton Ashmarin <aashmarin@gmail.com>
2011-04-10 12:12:05 +0000

    fix solution deconv added

--- RDA/FrmMain.Designer.cs
+++ RDA/FrmMain.Designer.cs
@@ -45,6 +45,8 @@
 			this.ucFuncAnalysis6 = new RDA.UcFuncAnalysis();
 			this.tabPage8 = new System.Windows.Forms.TabPage();
 			this.ucFuncAnalysis7 = new RDA.UcFuncAnalysis();
+			this.tabPage9 = new System.Windows.Forms.TabPage();
+			this.ucFuncAnalysis8 = new RDA.UcFuncAnalysis();
 			this.tabControl1.SuspendLayout();
 			this.tabPage1.SuspendLayout();
 			this.tabPage2.SuspendLayout();
@@ -54,6 +56,7 @@
 			this.tabPage6.SuspendLayout();
 			this.tabPage7.SuspendLayout();
 			this.tabPage8.SuspendLayout();
+			this.tabPage9.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tabControl1
@@ -66,12 +69,13 @@
 			this.tabControl1.Controls.Add(this.tabPage6);
 			this.tabControl1.Controls.Add(this.tabPage7);
 			this.tabControl1.Controls.Add(this.tabPage8);
+			this.tabControl1.Controls.Add(this.tabPage9);
 			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tabControl1.Location = new System.Drawing.Point(0, 0);
 			this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(911, 669);
+			this.tabControl1.Size = new System.Drawing.Size(936, 669);
 			this.tabControl1.TabIndex = 0;
 			// 
 			// tabPage1
@@ -81,7 +85,7 @@
 			this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-			this.tabPage1.Size = new System.Drawing.Size(903, 640);
+			this.tabPage1.Size = new System.Drawing.Size(928, 640);
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "График 4";
 			this.tabPage1.UseVisualStyleBackColor = true;
@@ -91,7 +95,7 @@
 			this.funcAnalysis1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.funcAnalysis1.Location = new System.Drawing.Point(3, 2);
 			this.funcAnalysis1.Name = "funcAnalysis1";
-			this.funcAnalysis1.Size = new System.Drawing.Size(897, 636);
+			this.funcAnalysis1.Size = new System.Drawing.Size(922, 636);
 			this.funcAnalysis1.TabIndex = 0;
 			// 
 			// tabPage2
@@ -101,7 +105,7 @@
 			this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.tabPage2.Name = "tabPage2";
 			this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-			this.tabPage2.Size = new System.Drawing.Size(903, 640);
+			this.tabPage2.Size = new System.Drawing.Size(928, 640);
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "График 2";
 			this.tabPage2.UseVisualStyleBackColor = true;
@@ -111,7 +115,7 @@
 			this.ucFuncAnalysis1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.ucFuncAnalysis1.Location = new System.Drawing.Point(3, 2);
 			this.ucFuncAnalysis1.Name = "ucFuncAnalysis1";
-			this.ucFuncAnalysis1.Size = new System.Drawing.Size(897, 636);
+			this.ucFuncAnalysis1.Size = new System.Drawing.Size(922, 636);
 			this.ucFuncAnalysis1.TabIndex = 1;
 			// 
 			// tabPage3
@@ -121,7 +125,7 @@
 			this.tabPage3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.tabPage3.Name = "tabPage3";
 			this.tabPage3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-			this.tabPage3.Size = new System.Drawing.Size(903, 640);
+			this.tabPage3.Size = new System.Drawing.Size(928, 640);
 			this.tabPage3.TabIndex = 2;
 			this.tabPage3.Text = "График 3";
 			this.tabPage3.UseVisualStyleBackColor = true;
@@ -131,7 +135,7 @@
 			this.ucFuncAnalysis2.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.ucFuncAnalysis2.Location = new System.Drawing.Point(3, 2);
 			this.ucFuncAnalysis2.Name = "ucFuncAnalysis2";
-			this.ucFuncAnalysis2.Size = new System.Drawing.Size(897, 636);
+			this.ucFuncAnalysis2.Size = new System.Drawing.Size(922, 636);
 			this.ucFuncAnalysis2.TabIndex = 1;
 			// 
 			// tabPage4
@@ -140,7 +144,7 @@
 			this.tabPage4.Location = new System.Drawing.Point(4, 25);
 			this.tabPage4.Name = "tabPage4";
 			this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage4.Size = new System.Drawing.Size(903, 640);
+			this.tabPage4.Size = new System.Drawing.Size(928, 640);
 			this.tabPage4.TabIndex = 3;
 			this.tabPage4.Text = "Полигармонический Процесс";
 			this.tabPage4.UseVisualStyleBackColor = true;
@@ -150,7 +154,7 @@
 			this.ucFuncAnalysis3.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.ucFuncAnalysis3.Location = new System.Drawing.Point(3, 3);
 			this.ucFuncAnalysis3.Name = "ucFuncAnalysis3";
-			this.ucFuncAnalysis3.Size = new System.Drawing.Size(897, 634);
+			this.ucFuncAnalysis3.Size = new System.Drawing.Size(922, 634);
 			this.ucFuncAnalysis3.TabIndex = 2;
 			// 
 			// tabPage5
@@ -159,7 +163,7 @@
 			this.tabPage5.Location = new System.Drawing.Point(4, 25);
 			this.tabPage5.Name = "tabPage5";
 			this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage5.Size = new System.Drawing.Size(903, 640);
+			this.tabPage5.Size = new System.Drawing.Size(928, 640);
 			this.tabPage5.TabIndex = 4;
 			this.tabPage5.Text = "Гармонический Процесс";
 			this.tabPage5.UseVisualStyleBackColor = true;
@@ -169,7 +173,7 @@
 			this.ucFuncAnalysis4.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.ucFuncAnalysis4.Location = new System.Drawing.Point(3, 3);
 			this.ucFuncAnalysis4.Name = "ucFuncAnalysis4";
-			this.ucFuncAnalysis4.Size = new System.Drawing.Size(897, 634);
+			this.ucFuncAnalysis4.Size = new System.Drawing.Size(922, 634);
 			this.ucFuncAnalysis4.TabIndex = 3;
 			// 
 			// tabPage6
@@ -178,7 +182,7 @@
 			this.tabPage6.Location = new System.Drawing.Point(4, 25);
 			this.tabPage6.Name = "tabPage6";
 			this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage6.Size = new System.Drawing.Size(903, 640);
+			this.tabPage6.Size = new System.Drawing.Size(928, 640);
 			this.tabPage6.TabIndex = 5;
 			this.tabPage6.Text = "Шум 1";
 			this.tabPage6.UseVisualStyleBackColor = true;
@@ -188,7 +192,7 @@
 			this.ucFuncAnalysis5.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.ucFuncAnalysis5.Location = new System.Drawing.Point(3, 3);
 			this.ucFuncAnalysis5.Name = "ucFuncAnalysis5";
-			this.ucFuncAnalysis5.Size = new System.Drawing.Size(897, 634);
+			this.ucFuncAnalysis5.Size = new System.Drawing.Size(922, 634);
 			this.ucFuncAnalysis5.TabIndex = 3;
 			// 
 			// tabPage7
@@ -197,7 +201,7 @@
 			this.tabPage7.Location = new System.Drawing.Point(4, 25);
 			this.tabPage7.Name = "tabPage7";
 			this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage7.Size = new System.Drawing.Size(903, 640);
+			this.tabPage7.Size = new System.Drawing.Size(928, 640);
 			this.tabPage7.TabIndex = 6;
 			this.tabPage7.Text = "Шум 2";
 			this.tabPage7.UseVisualStyleBackColor = true;
@@ -207,7 +211,7 @@
 			this.ucFuncAnalysis6.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.ucFuncAnalysis6.Location = new System.Drawing.Point(3, 3);
 			this.ucFuncAnalysis6.Name = "ucFuncAnalysis6";
-			this.ucFuncAnalysis6.Size = new System.Drawing.Size(897, 634);
+			this.ucFuncAnalysis6.Size = new System.Drawing.Size(922, 634);
 			this.ucFuncAnalysis6.TabIndex = 3;
 			// 
 			// tabPage8
@@ -216,7 +220,7 @@
 			this.tabPage8.Location = new System.Drawing.Point(4, 25);
 			this.tabPage8.Name = "tabPage8";
 			this.tabPage8.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage8.Size = new System.Drawing.Size(903, 640);
+			this.tabPage8.Size = new System.Drawing.Size(928, 640);
 			this.tabPage8.TabIndex = 7;
 			this.tabPage8.Text = "Кардиограмма";
 			this.tabPage8.UseVisualStyleBackColor = true;
@@ -226,14 +230,33 @@
 			this.ucFuncAnalysis7.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.ucFuncAnalysis7.Location = new System.Drawing.Point(3, 3);
 			this.ucFuncAnalysis7.Name = "ucFuncAnalysis7";
-			this.ucFuncAnalysis7.Size = new System.Drawing.Size(897, 634);
+			this.ucFuncAnalysis7.Size = new System.Drawing.Size(922, 634);
 			this.ucFuncAnalysis7.TabIndex = 4;
+			// 
+			// tabPage9
+			// 
+			this.tabPage9.Controls.Add(this.ucFuncAnalysis8);
+			this.tabPage9.Location = new System.Drawing.Point(4, 25);
+			this.tabPage9.Name = "tabPage9";
+			this.tabPage9.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage9.Size = new System.Drawing.Size(928, 640);
+			this.tabPage9.TabIndex = 8;
+			this.tabPage9.Text = "Деконволюця";
+			this.tabPage9.UseVisualStyleBackColor = true;
+			// 
+			// ucFuncAnalysis8
+			// 
+			this.ucFuncAnalysis8.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ucFuncAnalysis8.Location = new System.Drawing.Point(3, 3);
+			this.ucFuncAnalysis8.Name = "ucFuncAnalysis8";
+			this.ucFuncAnalysis8.Size = new System.Drawing.Size(922, 634);
+			this.ucFuncAnalysis8.TabIndex = 5;
 			// 
 			// FrmMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(911, 669);
+			this.ClientSize = new System.Drawing.Size(936, 669);
 			this.Controls.Add(this.tabControl1);
 			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.Name = "FrmMain";
@@ -247,6 +270,7 @@
 			this.tabPage6.ResumeLayout(false);
 			this.tabPage7.ResumeLayout(false);
 			this.tabPage8.ResumeLayout(false);
+			this.tabPage9.ResumeLayout(false);
 			this.ResumeLayout(false);
 
         }
@@ -270,6 +294,8 @@
 		private UcFuncAnalysis ucFuncAnalysis6;
 		private System.Windows.Forms.TabPage tabPage8;
 		private UcFuncAnalysis ucFuncAnalysis7;
+		private System.Windows.Forms.TabPage tabPage9;
+		private UcFuncAnalysis ucFuncAnalysis8;
     }
 }
 
--- RDA/convolution.cs
+++ RDA/convolution.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace RDA
 {
-	class Cardio
+	class Convolution
 	{
 		private const double Dt = 0.005;
 		private const double Tx = 5;
-		private const int Nx = (int) (Tx / Dt);
-		private const int R = (int) (1 / Dt);
+		private const int Nx = (int)(Tx / Dt);
+		private const int R = (int)(1 / Dt);
 		private const double Tp = 0.25;
-		private const int Np = (int) (Tp / Dt);
+		private const int Np = (int)(Tp / Dt);
 		private const double Arg = 2 * Math.PI * Dt;
 		private const double PsistAvg = 120;
 		private const double PdiastAvg = 80;
@@ -20,10 +17,9 @@ namespace RDA
 
 		private static readonly Random Rand = new Random();
 
-		static double[] Fh()
+		public static double[] Fh()
 		{
-			var res = new double[Np];
-
+			var res = new double[Ny];
 			for (int i = 0; i < Np; i++)
 				res[i] = Math.Sin(Arg * i / Tp) * Math.Exp(-0.1 * i);
 			return res;
@@ -31,16 +27,14 @@ namespace RDA
 
 		static double[] Fx()
 		{
-			var res = new double[Nx];
+			var res = new double[Ny];
 
 			for (int i = 0; i < Nx; i++)
 			{
-				if (i % R == 0)
-					res[i] = PsistAvg + (1 - Rand.NextDouble())*PsistAvg*0.05;
-				else if ((i - Tp*R) % R == 0)
+				if (i % R == 0 && i / R > 0)
+					res[i] = PsistAvg + (1 - Rand.NextDouble()) * PsistAvg * 0.05;
+				if ((i - Tp * R) % R == 0 && (i - Tp * R) / R > 0)
 					res[i] = -PdiastAvg + (1 - Rand.NextDouble()) * PdiastAvg * 0.05;
-				else
-					res[i] = 0;
 			}
 
 			return res;
@@ -52,15 +46,20 @@ namespace RDA
 			var x = Fx();
 			var h = Fh();
 
-			for (int i = 0; i < Ny && i < Nx; i++)
+			for (int i = 0; i < Ny; i++)
 			{
 				double t = 0;
-				for (int j = 0; j < i && j < Np; j++)
+				for (int j = 0; j < i; j++)
 					t += h[j] * x[i - j];
 				y[i] = t;
 			}
-			
-			return y;
+
+			var res = new double[FuncHelper.N];
+
+			for (int i = 0, j = Np / 2; i < FuncHelper.N; i++, j++)
+				res[i] = y[j];
+
+			return res;
 		}
 	}
 }
