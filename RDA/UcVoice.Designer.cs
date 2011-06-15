3434e55994462f66264fbd381b9cc8ecfd75c03a
anton <anton@6663979e-ad63-4aac-b921-a173afbfec27>
2011-06-15 20:22:34 +0000

    audio

---
+++ RDA/UcVoice.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace RDA
+{
+	partial class UcVoice
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.ucFilter1 = new RDA.UcFilter();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// ucFilter1
+			// 
+			this.ucFilter1.Location = new System.Drawing.Point(3, 3);
+			this.ucFilter1.Name = "ucFilter1";
+			this.ucFilter1.Size = new System.Drawing.Size(373, 251);
+			this.ucFilter1.TabIndex = 0;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(123, 260);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(351, 24);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(123, 320);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(78, 30);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "record";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(207, 320);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(78, 30);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "stop";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(291, 320);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(78, 30);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "play";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(3, 320);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(64, 45);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "apply filter";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(123, 290);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(351, 24);
+			this.comboBox2.TabIndex = 6;
+			this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(8, 267);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(73, 17);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "rec device";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(8, 297);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(109, 17);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "playback device";
+			// 
+			// UcVoice
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.ucFilter1);
+			this.Name = "UcVoice";
+			this.Size = new System.Drawing.Size(543, 463);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private UcFilter ucFilter1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
